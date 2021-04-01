a4f92eba7bc44a6b9aa7e7167c966c63f6401612
audrey bramy <audrey.bramy@gmail.com>
2021-04-01 09:45:37 +0200

    return a promise

--- scripts/createSchema.js
+++ scripts/createSchema.js
@@ -1,9 +1,20 @@
-const questionnaire = require('./questionnaire.js')
 const fs = require('fs')
 
-const importList = 'const mongoose = require(\'mongoose\');\n\n'
-let mongoSchemaStr = 'const mongoSchema = {\n' +
-` email: {
+const createAttribute = (name, type, isRequired) => {
+  if (isRequired !== undefined && isRequired === true) {
+    return `  ${name}: {
+        type: ${type},
+        required: true
+      }, 
+    \n`
+  } else {
+    return `  ${name}: ${type}, \n`
+  }
+}
+
+let mongoSchemaStr = `
+export default answerSchema = {
+  email: {
     type: String,
     required: true,
     unique: true
@@ -22,40 +33,32 @@ let mongoSchemaStr = 'const mongoSchema = {\n' +
     type: String,
     required: true,
     default: false
-  }, \n`
+  },\n`
 
-const createAttribute = (name, type, isRequired) => {
-  if (isRequired !== undefined && isRequired === true) {
-    return `  ${name}: {
-        type: ${type},
-        required: true
-      }, 
-    \n`
-  } else {
-    return `  ${name}: ${type}, \n`
-  }
-}
-const pages = questionnaire.pages
-pages.forEach(page => {
-  page.elements.forEach((element, i) => {
-    const name = element.name
-    const type = 'String'
-    if (element.type === 'matrix') {
-      // this is an object
-      let object = `const Object${i} = new mongoose.Schema({\n`
-      element.rows.forEach(row => {
-        object += `  "${row.replace(' ', '')}": String,\n`
+module.exports = (questionnaire) => {
+  return new Promise((resolve, reject) => {
+    const importList = 'import mongoose from \'mongoose\';\n\n'
+    questionnaire.pages.forEach(page => {
+      page.elements.forEach((element, i) => {
+        const name = element.name
+        const type = 'String'
+        if (element.type === 'matrix') {
+          let object = `const Object${i} = new mongoose.Schema({\n`
+          element.rows.forEach(row => {
+            object += `  "${row.replace(' ', '')}": String,\n`
+          })
+          object += '}); \n\n'
+          mongoSchemaStr = object + mongoSchemaStr
+          mongoSchemaStr += createAttribute(name, `Object${i}`)
+        } else {
+          mongoSchemaStr += createAttribute(name, type)
+        }
       })
-      object += '}); \n\n'
-      mongoSchemaStr = object + mongoSchemaStr
-      mongoSchemaStr += createAttribute(name, `Object${i}`)
-    } else {
-      mongoSchemaStr += createAttribute(name, type)
-    }
-  })
-})
+    })
 
-fs.writeFile('./schema/answer.js', importList + mongoSchemaStr + '}; \n\n module.exports = mongoSchema;', (err) => {
-  if (err) throw err
-  console.log('mongoSchema.js has been saved!')
-})
+    fs.writeFile('../src/db/Answer.js', importList + mongoSchemaStr + '};', (err) => {
+      if (err) { reject(err) }
+      resolve(true)
+    })
+  })
+}
