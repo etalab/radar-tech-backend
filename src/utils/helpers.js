0f9c71729e67b0e88aeac83e254720bbfc7902a4
audrey bramy <audrey.bramy@gmail.com>
2021-04-07 15:13:48 +0200

    remove console log

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,6 @@ const crypto = require('crypto')
 
 // Move this function in an other file
 const createHash = (toHash, salt) => {
-  console.log(toHash)
-  console.log(salt)
   return crypto.pbkdf2Sync(toHash, Buffer.from(salt, 'binary'), 1000, 32, 'sha512').toString('hex')
 }
 
