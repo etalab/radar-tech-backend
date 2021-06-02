b1419bfa80f0812227e7ec7a71056bc2a96d2683
audrey bramy <audrey.bramy@gmail.com>
2021-06-02 15:45:52 +0200

    display hash and salt

--- scripts/createHashAndSalt.js
+++ scripts/createHashAndSalt.js
@@ -5,5 +5,5 @@ const { createSalt, createHash } = require('../src/utils/helpers.js')
  */
 const myArgs = process.argv.slice(2)
 const salt = createSalt()
-console.log(salt)
-console.log(createHash(myArgs[1], salt))
+console.log(`Salt: ${salt}`)
+console.log(`Hash: ${createHash(myArgs[1], salt)}`)
